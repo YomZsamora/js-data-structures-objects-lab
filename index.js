4f917f01f79c1215328b1ad6ef0aa834cd8e3a09
yomZsamora <samora.y@adzumi.co.ke>
2022-01-19 15:55:06 +0300

    Returns a new driver that has an updated value for the key passed in.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // Defined a driver variable and assign it to an Object. Updates will be applied to this variable (destructively and non-destructively).
 const driver = {};
+
+// Returns a new driver that has an updated value for the key passed in
+let updateDriverWithKeyAndValue = (driverObject, key, value) => {
+   return Object.assign({}, driverObject, { [key]: value })
+}
